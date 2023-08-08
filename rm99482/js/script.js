10e06745c79718a579ce5d4718e0e89fff06b10f
PedroZilves <rm99482@fiap.com.br>
2023-08-08 11:27:38 -0300

    Manipulando lista e elementos html append e create

--- rm99482/js/script.js
+++ rm99482/js/script.js
@@ -23,3 +23,28 @@ for (let i= 0;i<nomesAlunos.length;++i){
 nomesAlunos.forEach((nome,index)=>{
     console.log(`Nome do aluno : ${index + 1}- ${nome}`)
 })
+const divLista = document.getElementById("Lista-nomes")
+//Gerar um novv loop para   iterar array e ao mesmo tempo
+// 1  - criar novos elementos na div
+//2  - adicionar cada um dos elementos do arrya a cada um dos elementos da div
+for (const nome of nomesAlunos){
+    let p = document.createElement("p")
+    p.textContent = nome
+    divLista.appendChild(p);
+    console.log("\n")
+
+    console.log(nome);
+    console.log("\n")
+}
+
+//Inserir novos nomes no array utilizando os metodos push e unshift
+//push() -> Insere um ou mais itens no ao final do array
+// unshift() no inicio
+
+nomesAlunos.push("André")
+nomesAlunos.unshift("Hugo")
+console.log("\n")
+nomesAlunos.shift()
+console.log("Array apos modificações",nomesAlunos)
+console.log("\n")
+//Removendo primeiro element com shift
