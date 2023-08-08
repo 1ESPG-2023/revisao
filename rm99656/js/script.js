a6b3f17b3c2299a707a0e319e3993387f9e9c56d
joaorihanfiap <rm99656@fiap.com.br>
2023-08-08 11:06:10 -0300

    commitzinho de cria

---
+++ rm99656/js/script.js
@@ -0,0 +1,21 @@
+
+
+let nomesAlunos = ["João", "Fernando", "Rodgrigo", "Matheus", "Eric"]
+
+// console.log(nomesAlunos[0]);
+
+// nomesAlunos.forEach(element => {
+//     console.log[i]
+// });
+
+for (var i = 0; i< nomesAlunos.length; i++){
+
+    let elemento = nomesAlunos[i];
+
+    console.log(`Elemento ${i}: `,elemento);
+}
+
+
+nomesAlunos.forEach(nome =>{
+    console.log(`Nome do cria: ${nome}`)
+})
