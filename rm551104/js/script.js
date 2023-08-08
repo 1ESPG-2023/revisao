57ef24c6d32ae1adcf8e1e14249a9695ab155a9e
fernandakaory <rm551104@fiap.com.br>
2023-08-08 11:27:50 -0300

    Manipulando  lista e elementos html com append

--- rm551104/js/script.js
+++ rm551104/js/script.js
@@ -28,3 +28,43 @@ nomesAlunos.forEach((nome, index) =>{
     console.log(`Nome do aluno: ${index+1} - ${nome}`);
 });
 
+//Quebrar linha
+console.log("\n")
+
+//Gerar um novo loop para iterar o array e ao mesmo tempo
+//1- Criar novos elementos na div
+//2- Adicionar cada um dos elementos do array a cada um dos elementos da div
+
+const divLista = document.getElementById("lista-nomes")
+
+//Utilizando o for of para iterar o array
+// o object na estrutura do for of é o array que vou utilizar
+for (const nome of nomesAlunos) {
+    //elemento p sendo criado
+    let p = document.createElement("p");
+    //Texto nome sendo inserido no elemento p
+    p.textContent = nome;
+    //Adicionando o elemtno p dentro da div lista
+    divLista.appendChild(p);
+
+    console.log(nome);
+    
+}
+
+//Quebrar linha
+console.log("\n")
+
+//Inserir novos nomes no array utilizando os métodos: push e unshift
+/*Push - insere um ou mais itens ao final fo array*/
+/*Unshift - Insere um ou mais itens no inicio do array*/
+nomesAlunos.push("André");
+nomesAlunos.unshift("Hugo");
+//Imprimindo o array depois das alterações realizadas nos métodos acima:
+console.log("Array após as modificações", nomesAlunos);
+
+//Quebrar linha
+console.log("\n")
+
+//Removendo o primeiro alemento com shift
+nomesAlunos.shift();
+console.log("Array após as modificações", nomesAlunos);
