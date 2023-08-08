7829f9ad4f8e4672a51963c7eda6a5cc6fa12411
Rodrigo Lima <rodrigo.blima17@gmail.com>
2023-08-08 11:27:57 -0300

    Manipulando lista e elementos html append

--- rm98326/js/script.js
+++ rm98326/js/script.js
@@ -1,5 +1,5 @@
 //Declaração de um array de nomes de alunos:
-let nomesAlunos = ["persia", "José", "Carlos", "Julia", "Claudia"]
+let nomesAlunos = ["Persia", "José", "Carlos", "Julia", "Claudia"]
 
 //Impressão da primeira posição do array:
 console.log(nomesAlunos[0])
@@ -27,3 +27,46 @@ nomesAlunos.forEach( (nome,index) =>{
     console.log(`Nome do Aluno: ${index + i} - ${nome}`)
 })
 
+//Quebrar linha:
+console.log("\n")
+
+//Gera um novo loop para iterar o array e ao mesmo tempo
+// 1 - Criar novos elementos na div
+// 2 - Adicionar cada um dos elementos do array a cada um dos elementos da div
+
+const divLista = document.getElementById("lista-nomes")
+
+//Utilizando o for of para iterar o array
+for(const nome of nomesAlunos) {
+    //Elemento p sendo criado
+    let p = document.createElement("p")
+
+    //Texto nome sendo inserido no elemento p
+    p.textContent = nome
+
+    //Adicionando o elemento p na div Lista 
+    divLista.appendChild(p)
+    
+    console.log(nome)
+}
+
+//Quebrar linha:
+console.log("\n")
+
+//Inserir novos nomes no array, utilizando os métodos: push e unshift
+/*push() -> Insere um ou mais itens no final do array*/
+/*unshift() -> Insere um ou mais itens no início do array*/
+nomesAlunos.push("André")
+nomesAlunos.unshift("Hugo")
+
+//Imprimindo o array
+console.log("Array após as modificações: ", nomesAlunos)
+
+//Quebrar linha:
+console.log("\n")
+
+//Removendo o primeiro elemento com shift
+nomesAlunos.shift()
+
+//Imprimindo o array
+console.log("Array após as modificações: ", nomesAlunos)
