02326a35b5eb2987b579f1c71a52d6211bc749c5
emnuelle <rm97973@fiap.com.br>
2023-08-14 15:17:57 -0300

    cemeçando os exercícios

--- rm97973/js/script.js
+++ rm97973/js/script.js
@@ -1,90 +1,131 @@
-// Declaração de uma array de nomes de alunos:
+
+//Declaração de um array de nomes de alunos:
 let nomesAlunos = ["Persia","José","Aline","Carlos","Júlia","Marcelo","Claudia"];
 
-// Impressão da primeira posição do array
+//Impressão da primeira posição do array:
 console.log(nomesAlunos[0]);
-// Quabrar a linha para a melhor vizualização no console
+//Quebrar linha para melhor visualização no console:
 console.log("\n");
 
-
-// Impriminto todos os elemtnos da Array com for: (tradicional utilizando o índice do array)
-for (var i= 0;i<nomesAlunos.length ;++i){
-    // Imprime o valor atual do elemento naq viavél "elemento"
+//Imprimindo todos os elementos do array com for:
+for (let i= 0;i<nomesAlunos.length ;++i){
+    //Imprime o valor atual do elemento na variável "elemento":
     let elemento = nomesAlunos[i];
-    // Imprimindo o elemento atual do array:
-    console.log("elemeto"+i+ ": ", elemento);
-    // Quebra linha
-    console.log("\n");
-    // Imprimindo o elemento atual do array com template String
-    console.log(`elemento $`, elemento);
-    // Quebra linha 
-    console.log("\n")
-    // Utilizar forEach para imprimir array
-    // nomesAlunos.forEach(nome,index=>function(){
-    //     console.log(`Nome do aluno: ${index} - ${nome}`);
-    // });
-};
-
-console.log("\n")
-
-//  Gerar um novo loop para iterar o array e ao mesmo tempo 
-//  1 - criar novos elementos na div 
-//  2- Adicionar cada um dos elementos do array a cada um dos elementos da div (html)
-
-const divLista = document.getElementById("lista-nomes");
-
-// Utilizando o for of para iterar p array
-for (const nome of nomesAlunos) {
-    // Elemento p sendo criado
-    let p = document.createElement("p");
-    // texto nome sendo inserido no elemento p 
-    p.textContent= nome;
-    // Adicionando o elemento p a div lista
-    divLista.appendChild(p);
+    //Imprimindo o elemento atual do array:
+    console.log("Elemento "+i+ ": ",elemento);
     
-    console.log(nome)
+    //Imprimindo o elemento atual do array com template String:
+    console.log(`Elemento ${i}: `,elemento);
+    //Quebrar linha
+    console.log("\n");
 }
 
-//  Inserir novos nomes no array, utilizando os métodos: ush e unshift
-// push() -> insere um ou mais itens no ao final do array
-// unshift() -> insere um ou mais intens no inicio do array
-nomesAlunos.push("André");
-nomesAlunos.unshift("Hugo");
-// mostrando a situação atual do array
-console.log('Array após modificações:',nomesAlunos)
+    //Quebrar linha
+    console.log("\n");
+
+    //Utilizar um forEach para imprimir o Array:
+    nomesAlunos.forEach( (nome,index) =>{
+        console.log(`Nome do aluno : ${index + 1} - ${nome}`);     
+    });
+    
+    //Quebrar linha
+    console.log("\n");
+
+    //Gerar um novo loop para iterar o array e ao mesmo tempo
+    // 1 - Criar novos elementos na div
+    // 2 - Adicionar cada um dos elementos do array a cada um dos elementos da div
+
+    const divLista = document.getElementById("lista-nomes");
+
+    //Utilizando o for of para iterar o array
+    for (const nome of nomesAlunos) {
+        //Elemento p sendo criando
+        let p = document.createElement("p");
+        //Texto nome sendo inserido no elemento p
+        p.textContent = nome;
+        //Adicionando o elemento p dentro da div lista
+        divLista.appendChild(p);
+
+        console.log(nome);
+
+    }
+
+    //Quebrar linha
+    console.log("\n");
+
+    //Inserir novos nomes no array, utilizando os metodos: push e unshift
+    /*push() -> Insere um ou mais itens no ao final array */
+    /*unshift() -> Insere um ou mais itens no início do array */
+    nomesAlunos.push("André");
+    nomesAlunos.unshift("Hugo");
+    //Imprimindo o array depois das alterações realizadas nos métodos acima:
+    console.log('Array após as modificações:',nomesAlunos );
+
+    //Quebrar linha
+    console.log("\n");
+    //Removendo o primeiro elemento com shift
+    nomesAlunos.shift();
+    //Imprimindo o array depois das alterações realizadas nos métodos acima:
+    console.log('Array após as modificações:',nomesAlunos );
+
+    //Quebrar linha
+    console.log("\n");
+    //Removendo o último elemento com pop
+    nomesAlunos.pop();
+    //Imprimindo o array depois das alterações realizadas nos métodos acima:
+    console.log('Array após as modificações:',nomesAlunos );
+
+    //Quebrar linha
+    console.log("\n");
+    //Colocando os dados em ordem alfabética com o método sort.
+    nomesAlunos.sort();
+    //Imprimindo o array depois das alterações realizadas nos métodos acima:
+    console.log('Array após as modificações:',nomesAlunos );
+
+    //Quebrar linha
+    console.log("\n");
+    //Revertendo a posição dos dados com o método reverse.
+    nomesAlunos.reverse();
+    //Imprimindo o array depois das alterações realizadas nos métodos acima:
+    console.log('Array após as modificações:',nomesAlunos );
+
+    //Quebrar linha
+    console.log("\n");
+    //Buscando elementos com indexOf
+    let index = nomesAlunos.indexOf("XXX");
+    //Imprimindo o array depois das alterações realizadas nos métodos acima:
+    console.log('Array após as modificações:',nomesAlunos );
+    console.log('Posição onde o elemento foi encontrado :', index );
+
+        //Quebrar linha
+        console.log("\n");
+        //Alterando um elemento no array com o método splice
+        //Imprimindo o array depois das alterações realizadas nos métodos acima:
+        nomesAlunos.splice(3,1,"Jojo");
+        console.log('Array após as modificações:',nomesAlunos );
+
+        //Quebrar linha
+        console.log("\n");
+        //Alterando um elemento no array com o método splice e solicitando mais uma alteração.
+        //Imprimindo o array depois das alterações realizadas nos métodos acima:
+        nomesAlunos.splice(3,2,"Tuca");
+        console.log('Array após as modificações:',nomesAlunos );
+
+    //Quebrar linha
+    console.log("\n");
+    //Remover um elemento no array com o método splice
+    //Imprimindo o array depois das alterações realizadas nos métodos acima:
+    nomesAlunos.splice(1,1);
+    console.log('Array após as modificações:',nomesAlunos );
 
 
-// Removendo o primiero elemento com o shift
-nomesAlunos.shift();
-// mostrando a situação atual 
-console.log('Array após modificações:',nomesAlunos);
-
-nomesAlunos.pop()
-console.log('Array após modificações:',nomesAlunos);
-
-nomesAlunos.sort()
-console.log('Array após modificações:',nomesAlunos);
-
-nomesAlunos.reverse()
-console.log('Array após modificações:',nomesAlunos);
-
-// Buscando elementos com indexOf
-let index = nomesAlunos.indexOf('claudia');
-console.log('Array após modificações:',nomesAlunos);
-console.log('Posição onde o elemtno foi encontrado:', index);
-
-// alterando um alemento no array com o método splice
-nomesAlunos.splice(3,1,"Jojo");
-console.log('Array após modificações:',nomesAlunos);
-
-// alterando o elemento no array com o mpetodo splice e solicitando mais uma alteração
-nomesAlunos.splice(3,2,"tuca");
-console.log('Array após modificações:',nomesAlunos); //a claudia sumiu :(
-
-// Removendo um elemento do array com o metodo splice utilizando metodo indexof como parametro 
-let indexNome = nomesAlunos.indexOf("Tuca");
-nomesAlunos.splice(indexNome,1);
-console.log('Array após modificações:',nomesAlunos);
+    //Quebrar linha
+    console.log("\n");
+    //Remover um elemento no array com o método splice utilizando o método indexOf como parâmetro.
+    //Imprimindo o array depois das alterações realizadas nos métodos acima:
+    let indexNome = nomesAlunos.indexOf("Tuca");
+    nomesAlunos.splice(indexNome,1);
+    console.log('Array após as modificações:',nomesAlunos );
 
 
-
+    
