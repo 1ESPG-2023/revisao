5c5268cbd5f8efb2b09e497132000324c74c7b41
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-08-15 09:32:34 -0300

    <Exercicio> Exercicio to Do done

--- exercicio-01-array/js/main.js
+++ exercicio-01-array/js/main.js
@@ -1,5 +1,6 @@
 const inputTarefa = document.getElementById("inputTarefa");
 const form = document.getElementById("formAddTarefa");
+const listaTarefas = document.getElementById("listaTarefas")
 const ul = document.getElementById("ul");
 let tarefas = [] //array
 
@@ -17,7 +18,23 @@ function adicionaTarefaNaLista(tarefas){
     tarefas.forEach((item) => {
         let li = document.createElement("li")
         li.textContent = item
+        li.classList.add("task")
+        let button = document.createElement("button")
+        button.textContent = "X"
+        button.addEventListener("click", ()=>{
+            deletaItem(item)
+        })
+        li.appendChild(button)        
         ul.appendChild(li)
         
    });
+   listaTarefas.appendChild(ul)
+}
+
+function deletaItem(item){
+    const indexLista = tarefas.indexOf(item)
+    if (indexLista !== -1){
+        tarefas.splice(indexLista, 1)
+        adicionaTarefaNaLista(tarefas)
+    }
 }
