8db63fd96fd5d2ff0a0aba7a0c63761291a139d3
fernandakaory <rm551104@fiap.com.br>
2023-08-15 11:56:32 -0300

    usando indexof e splice

--- rm551104/exercicio1-correcao/js/tarefa-correcao1.js
+++ rm551104/exercicio1-correcao/js/tarefa-correcao1.js
@@ -29,8 +29,42 @@ botaoAddTarefa.addEventListener("click", (evento)=>{
     li.appendChild(botaoExcluir); 
 
     listaTarefasUl.appendChild(li);
+    // divisao
+
+    botaoExcluir.addEventListener("click", (evento)=>{
+        evento.preventDefault();
+        // usando parentNode pois o Li que precisa ser exluido é pai do botao clicado
+        // console lo (botao.paidobotao.Textopaidobotao)
+        // console.log(evento.target.parentNode.textContent);
+
+        // Pegando o texto da li dividindo com split
+        // é o tolken (",")
+        let conteudoDaLi = evento.target.parentNode.textContent.split(" ");
+        // console.log(conteudoDaLi);
+
+        //Descobrindo o índice da tarefa com indexOf
+        let indiceTarefa = listaTarefasArray.indexOf(conteudoDaLi[0]);
+        // console.log(indiceTarefa)
+        
+        // Remeovendo do array com o método splice passando o indice.
+        listaTarefasArray.splice(indiceTarefa, 1);
+
+
+        evento.target.parentNode.remove();
+
+        // imprimindo o array e limpando campo
+        console.log(listaTarefasArray);
+
+    })
 
     // imprimindo o array e limpando campo
     console.log(listaTarefasArray)
     inputTarefa.value=""
 })
+
+// Entendendo o split
+let frase = "Meu prof, de, js é lindo e, eu, amo ele."
+console.log(`Frase ${frase}`)
+
+let fraseSplitada = frase.split(",");
+console.log(fraseSplitada)
