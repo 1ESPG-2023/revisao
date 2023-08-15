b6b5ef0b5694873cf6c556f15849e447a6bc45c9
GabrielMachadoCP <rm99880@fiap.com.br>
2023-08-15 11:18:55 -0300

    Jeito preguiçoso de criar o li

--- rm99880/js/tarefa.js
+++ rm99880/js/tarefa.js
@@ -1,1 +1,24 @@
 let listaTarefasArray = [];
+
+const botaoAddTarefa = document.querySelector("#btnAddTarefa");
+
+botaoAddTarefa.addEventListener("click", (evt)=>{
+
+    //Prevenindo o reload da página.
+    evt.preventDefault();
+
+    //Recuperando o input da tarefa
+    const inputTarefa = document.querySelector("#idTarefa");
+
+    //Inerindo a tarefa no array de tarefas.
+    listaTarefasArray.push(inputTarefa.value);
+    
+    const listaTarefasUL = document.querySelector("#listaTarefa");
+    let li = document.createElement("li");
+    li.innerHTML = inputTarefa.value + ""+ "<button> x </button>";
+    listaTarefasUL.appendChild(li);
+
+    //Imprimindo o array de tarefas e limpando o campo.
+    console.log(listaTarefasArray);
+    inputTarefa.value = "";
+});
