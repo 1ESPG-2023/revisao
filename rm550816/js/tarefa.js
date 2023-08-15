f85badb6787e1d7eed22fe219a2927bfb91b9e96
Rodrigooou <rm550816@fiap.com.br>
2023-08-15 11:24:03 -0300

    Inserindo tarefas na lista ul-buttons createElement

--- rm550816/js/tarefa.js
+++ rm550816/js/tarefa.js
@@ -15,6 +15,22 @@ botaoAddTarefa.addEventListener("click", (evt)=>{
     // .value ele pega o valor do objeto: aparecendo a palavra corretamente no console
     listaTarefasArray.push(inputTarefa.value); 
 
+    const listaTarefasUL = document.querySelector("#listaTarefa");
+
+    let li = document.createElement("li")
+    
+    // li.innerHTML = inputTarefa.value + " " + "<button> x </button>"; JEITO MAL FEITO
+
+    li.textContent = inputTarefa.value;
+
+    let botaoExcluir = document.createElement("button");
+
+    botaoExcluir.textContent = " x ";
+
+    li.appendChild(botaoExcluir);
+
+    listaTarefasUL.appendChild(li); 
+
     //Imprimindo o array de tarefas e limpando o campo.
     console.log(listaTarefasArray);
     inputTarefa.value = ""; //Serve pra limpar o campo
