6aba75082b25e7d2a5d4418beb94877bd072300b
LanaAuada <rm551143@fiap.com.br>
2023-08-15 11:56:32 -0300

    indexOf e splice

--- rm551143/js/tarefa.js
+++ rm551143/js/tarefa.js
@@ -21,7 +21,7 @@ botaoAddTarefa.addEventListener("click", (evt)=>{
 
     li.textContent = inputTarefa.value;
 
-    let botaoExcluir = document.createElement("button")
+    let botaoExcluir = document.createElement("button");
 
     botaoExcluir.textContent = " x ";
 
@@ -29,8 +29,37 @@ botaoAddTarefa.addEventListener("click", (evt)=>{
 
     listaTarefasUL.appendChild(li);
 
+    botaoExcluir.addEventListener("click", (evt)=>{
+        evt.preventDefault();
+
+        // console.log(botao.paidoBotao.textodoPaidoBotao);
+        // console.log(evt.target.parentNode.textContent);
+
+        //pegando o texto da li dividindo com o split
+        let conteudoDaLi = evt.target.parentNode.textContent.split(" ");
+        // console.log(conteudoDaLi)
+        //descobrindo o indice da tarefa com indexOf
+        let indiceTarefa = listaTarefasArray.indexOf(conteudoDaLi[0]);
+        // console.log(indiceTarefa);
+        
+        //removendo o array com metodo splice passando o indice
+        listaTarefasArray.splice(indiceTarefa, 1);
+
+        evt.target.parentNode.remove();
+        //imprimindo o array de tarefas e limpando o campo
+        console.log(listaTarefasArray);
+    });
+
     //imprimindo o array e tarefas e limpando o campo
     console.log(listaTarefasArray);
     inputTarefa.value = "";
 
-})
+});
+
+
+//entendendo o método SPLIT de string em JS
+let frase = "Meu prof, de, JS é lindo e, eu, amo ele.";
+console.log(`Frase: ${frase}`);
+
+let fraseSplitada = frase.split(",");
+console.log(fraseSplitada);
