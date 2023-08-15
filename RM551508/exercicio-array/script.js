926062eb5b9a86457b12f384803e42847a6e2047
EnzoMShiotuqui <RM551508@fiap.com>
2023-08-15 11:23:33 -0300

    criando botoes

--- RM551508/exercicio-array/script.js
+++ RM551508/exercicio-array/script.js
@@ -7,8 +7,21 @@ botaoAddTarefa.addEventListener("click", (evento)=>{
     evento.preventDefault()
     const InputTarefa = document.querySelector("#IdTarefa")
     listaTarefasArray.push(InputTarefa.value)
+    
+    
+    const listaTarefaUl = document.querySelector("#listaTarefa")
+    let li = document.createElement("li")
+    
+    li.textContent = InputTarefa.value
+
+    let botaoExcluir = document.createElement("button")
+    botaoExcluir.textContent =  " x "
+
+    li.appendChild(botaoExcluir)
+    listaTarefaUl.appendChild(li)
+
+
+
     console.log(listaTarefasArray)
     InputTarefa.value =  ''
-
 })
-
