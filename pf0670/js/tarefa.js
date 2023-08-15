7d91e49b663e77fc8676ac7e1a3da66f5ebc70ac
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-15 11:24:07 -0300

    Inserindo tarefas  na lista ul+buttons. createElements.

--- pf0670/js/tarefa.js
+++ pf0670/js/tarefa.js
@@ -14,6 +14,25 @@ botaoAddTarefa.addEventListener("click", (evt)=>{
     //Inerindo a tarefa no array de tarefas.
     listaTarefasArray.push(inputTarefa.value);
     
+    const listaTarefasUL = document.querySelector("#listaTarefa");
+
+    let li = document.createElement("li"); 
+
+    // li.innerHTML = inputTarefa.value +" <button> x </button>";
+
+    li.textContent = inputTarefa.value;
+
+    let botaoExcluir = document.createElement("button");
+
+    botaoExcluir.textContent = " x ";
+
+    li.appendChild(botaoExcluir);
+
+    listaTarefasUL.appendChild(li);
+
+
+
+    
     //Imprimindo o array de tarefas e limpando o campo.
     console.log(listaTarefasArray);
     inputTarefa.value = "";
