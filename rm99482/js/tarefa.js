f4708c0be46781e54b4f997fbacb795b81792868
PedroZilves <rm99482@fiap.com.br>
2023-08-15 11:24:27 -0300

    Segunda parte pronta

--- rm99482/js/tarefa.js
+++ rm99482/js/tarefa.js
@@ -2,14 +2,34 @@ let listaTarefasArray = [];
 
 const botaoAddTarefa = document.querySelector("#btnAddTarefa");
 
-botaoAddTarefa.addEventListener("click",(evento)=>{
-   
-    evento.preventDefault();
+botaoAddTarefa.addEventListener("click", (evt)=>{
 
+    //prevenindo o reload da página
+    evt.preventDefault();
 
+    //Recuperando o input da tarefa
     const inputTarefa = document.querySelector("#idTarefa");
-    console.log(inputTarefa.value)
-    console.log(listaTarefasArray)
-    inputTarefa.value="";
-    
-})
+
+    //inserindo a tarefa no array
+    listaTarefasArray.push(inputTarefa.value);
+
+    const listaTarefasUl = document.querySelector("#listaTarefa");
+
+    let li = document.createElement("li");
+
+    // li.innerHTML = inputTarefa.value +" <button> x </button>";
+
+    li.textContent = inputTarefa.value;
+
+    let botaoExcluir = document.createElement("button");
+
+    botaoExcluir.textContent = " x ";
+
+    li.appendChild(botaoExcluir);
+
+    listaTarefasUl.appendChild(li);
+
+    console.log(listaTarefasArray);
+    inputTarefa.value = "";
+
+});
