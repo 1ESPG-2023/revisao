c13697108dae466102583bb217c8c40e412632a5
BernardoPRocha <RM99209@fiap.com.br>
2023-09-12 11:21:56 -0300

    cp4

--- rm99209/CP4/cp4.js
+++ rm99209/CP4/cp4.js
@@ -25,3 +25,83 @@ console.log("Novos salários:", novosSalarios);
 console.log("Salários acima de 2500:", salariosSuperiores2500);
 console.log("Soma dos salários acima de 2500:", somaSalarios);
 
+//-------------------------------------------------------------------------------------------//
+
+// O evento "DOMContentLoaded" é acionado quando a página HTML foi completamente carregada
+document.addEventListener("DOMContentLoaded", () => {
+  // Obtém referências aos elementos HTML que vamos usar
+  const tabelaTarefas = document.getElementById("tabelaTarefas");
+  const adicionarTarefaBtn = document.getElementById("adicionarTarefa");
+  
+  // Adiciona um ouvinte de evento ao botão "Adicionar Tarefa"
+  adicionarTarefaBtn.addEventListener("click", () => {
+    // Obtém os valores dos campos de entrada no formulário
+    const descricao = document.getElementById("descricao").value;
+    const autor = document.getElementById("autor").value;
+    const departamento = document.getElementById("departamento").value;
+    const importancia = parseInt(document.getElementById("importancia").value);
+    
+    // Chama a função para adicionar a tarefa ao array
+    adicionarTarefa(descricao, autor, departamento, importancia);
+    
+    // Atualiza a tabela com as tarefas
+    atualizarTabela();
+  });
+  
+  // Um array para armazenar as tarefas
+  const tarefas = [];
+  
+  // Função para adicionar uma nova tarefa ao array
+  function adicionarTarefa(descricao, autor, departamento, importancia) {
+    const tarefa = {
+      descricao,
+      autor,
+      departamento,
+      importancia,
+      concluida: false
+    };
+    tarefas.push(tarefa);
+  }
+  
+  // Função para atualizar a tabela de tarefas no HTML
+  function atualizarTabela() {
+    // Limpa a tabela antes de atualizar
+    tabelaTarefas.innerHTML = "";
+    
+    // Itera sobre as tarefas no array e adiciona cada uma na tabela
+    tarefas.forEach((tarefa, index) => {
+      const row = tabelaTarefas.insertRow();
+      
+      // Insere os valores das colunas na linha da tabela
+      row.insertCell().textContent = tarefa.descricao;
+      row.insertCell().textContent = tarefa.autor;
+      row.insertCell().textContent = tarefa.departamento;
+      row.insertCell().textContent = tarefa.importancia;
+      row.insertCell().textContent = tarefa.concluida ? "Sim" : "Não";
+      
+      // Cria um botão "Concluir" e adiciona um ouvinte de evento
+      const acaoCell = row.insertCell();
+      const concluirBtn = document.createElement("button");
+      concluirBtn.textContent = "Concluir";
+      concluirBtn.addEventListener("click", () => {
+        // Marca a tarefa como concluída quando o botão é clicado
+        tarefas[index].concluida = true;
+        // Atualiza a tabela após marcar como concluída
+        atualizarTabela();
+      });
+      acaoCell.appendChild(concluirBtn);
+
+      // Cria um botão "Excluir" e adiciona um ouvinte de evento
+    const excluirBtn = document.createElement("button");
+    excluirBtn.textContent = "Excluir";
+    excluirBtn.addEventListener("click", () => {
+      // Remove a tarefa do array quando o botão é clicado
+      tarefas.splice(index, 1);
+      // Atualiza a tabela após remover a tarefa
+      atualizarTabela();
+    });
+    acaoCell.appendChild(excluirBtn);
+
+    });
+  }
+});
